9d9e1fe03cc4e6f63e3b88110804c5aa10b1a06d
merisman <6125373+merisman@users.noreply.github.com>
2024-07-09 23:48:40 -0600

    Updating Auth transfers

--- src/libs/auth.sol
+++ src/libs/auth.sol
@@ -20,6 +20,7 @@ contract BorgAuth {
     uint256 public constant OWNER_ROLE = 99;
     uint256 public constant ADMIN_ROLE = 98;
     uint256 public constant PRIVILEGED_ROLE = 97;
+    address pendingOwner;
 
     //mappings and events
     mapping(address => uint256) public userRoles;
@@ -30,6 +31,8 @@ contract BorgAuth {
 
     /// @dev user not authorized with given role
     error BorgAuth_NotAuthorized(uint256 role, address user);
+    error BorgAuth_SetAnotherOwner();
+    error BorgAuth_ZeroAddress();
 
     /// @notice deployer is owner
     constructor() {
@@ -44,7 +47,31 @@ contract BorgAuth {
         uint256 role
     ) external {
          onlyRole(OWNER_ROLE, msg.sender);
+         if(user == msg.sender && role < OWNER_ROLE) revert BorgAuth_SetAnotherOwner();
         _updateRole(user, role);
+    }
+    
+    /// @notice initialize ownership transfer
+    /// @param newOwner address of new owner
+    function initTransferOwnership(address newOwner) external {
+        if (newOwner == address(0) || newOwner == msg.sender) revert BorgAuth_ZeroAddress();
+        onlyRole(OWNER_ROLE, msg.sender);
+        pendingOwner = newOwner;
+    }
+
+    /// @notice accept ownership transfer
+    function acceptOwnership() external {
+        if (msg.sender != pendingOwner) revert BorgAuth_NotAuthorized(OWNER_ROLE, msg.sender);
+        _updateRole(pendingOwner, OWNER_ROLE);
+        pendingOwner = address(0);
+        emit RoleUpdated(pendingOwner, OWNER_ROLE);
+    }
+
+    /// @notice function to purposefully revoke all roles from owner, rendering subsequent role updates impossible
+    /// @dev this function is intended for use to remove admin controls from subsequent contracts using this auth
+    function zeroOwner() external {
+        onlyRole(OWNER_ROLE, msg.sender);
+        _updateRole(msg.sender, 0);
     }
 
     /// @notice set adapter for role
